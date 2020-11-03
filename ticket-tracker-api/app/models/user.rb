ba93b2c2e13aab65f2161352bb1626149290ffbe
Rafael Batista <RafaelB2424@gmail.com>
2020-11-03 18:31:01 -0500

    Add method to handle login a user

--- ticket-tracker-api/app/models/user.rb
+++ ticket-tracker-api/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
     has_secure_password 
     has_many: :projects
+
+    def self.handle_login(email, password)
+        user = User.find_by(email: email.downcase)
+        
+        if user && user.authenticate(password) then return user end
+          
+       return false
+    end
 end
