25b5006d063401bf45cdff9faace6f87114301cf
Raf-Batista <RafaelB2424@gmail.com>
2020-11-02 20:56:02 -0800

    Create Navbar

---
+++ ticket-tracker-client/src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navigation d-lg-flex justify-content-lg-around">
+            <nav class="navbar navbar-expand-lg navbar-light ">
+                <a class="navbar-brand" href="#">Ticket Tracker</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Account</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Help</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
