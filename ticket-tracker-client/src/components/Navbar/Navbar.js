df1c97dc9577ebf53eda5c04e930346e82a75e4e
Rafael Batista <RafaelB2424@gmail.com>
2020-11-03 17:57:57 -0500

    Add class to home page link

--- ticket-tracker-client/src/components/Navbar/Navbar.js
+++ ticket-tracker-client/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className="navigation d-lg-flex justify-content-lg-around p-3">
             <nav class="navbar navbar-expand-lg navbar-light ">
-                <a class="navbar-brand" href="#">Ticket Tracker</a>
+                <a class="navbar-brand home-link" href="/">Ticket Tracker</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
