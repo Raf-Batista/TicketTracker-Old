ac556cce93a85635923dfa1cd9080b2b5dddca6f
Raf-Batista <RafaelB2424@gmail.com>
2020-11-02 20:54:07 -0800

    Create home page

---
+++ ticket-tracker-client/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="container text-center mt-5">
+            Home Page
+        </div>
+    )
+}
+
+export default Home
