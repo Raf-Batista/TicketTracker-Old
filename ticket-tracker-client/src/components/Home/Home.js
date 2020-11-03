db9f330a945bb335d5b53689f45921c022764a54
Rafael Batista <RafaelB2424@gmail.com>
2020-11-03 18:51:42 -0500

    Add description and links to login or signup

--- ticket-tracker-client/src/components/Home/Home.js
+++ ticket-tracker-client/src/components/Home/Home.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div className="container text-center mt-5">
-            Home Page
+        <div className="container text-center mt-5 home">
+            <h1>
+                <span className="mb-3">
+                    Welcome to Ticket Tracker
+                </span>
+                <span>
+                    Keep track of tickets you create and mark off completed tickets for your various projects
+                </span>
+            </h1>
+            <div className="account-links mt-5">
+                <Link to="/signup">Create an account</Link>
+                <Link to="login">Login</Link>
+            </div>
         </div>
     )
 }
