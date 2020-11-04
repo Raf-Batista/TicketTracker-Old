eadae194a0fc928b9a281df22fe1bbee1f973c56
Rafael Batista <RafaelB2424@gmail.com>
2020-11-04 13:29:32 -0500

    Refactor create action

--- ticket-tracker-api/app/controllers/users_controller.rb
+++ ticket-tracker-api/app/controllers/users_controller.rb
@@ -1,21 +1,22 @@
 class UsersController < ApplicationController
 
-    def create 
+    def create
         user = User.new(user_params)
 
-        if user.valid?
-            user.create 
+        if user.save 
             token = CoreModules::JsonWebToken.encode({user_id: user.id}, 4.hours.from_now)
             cookies.signed[:jwt] = {value: token, httponly: true}
-            render json: user, status :ok and return
-        end 
+            render json: user, status: :created and return
+        else 
+            render json: {errors: user.errors.full_messages}, status: :not_acceptable and return
+        end  
 
-        render json: {"error"}
+        render json: {errors: "An error occured"}, status: :not_acceptable
     end 
 
     private 
 
     def user_params 
-        params.permit(:user).require(:username, :email, :password)
+        params.permit(:username, :email, :password)
     end 
 end
