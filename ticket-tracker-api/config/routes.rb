1e4db355620da654e63e4b12e284d4ea750c6a67
Rafael Batista <RafaelB2424@gmail.com>
2020-11-05 00:25:34 -0500

    Update routes

--- ticket-tracker-api/config/routes.rb
+++ ticket-tracker-api/config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-    resources :users
+    resources :users do 
+      resources :projects
+    end 
 
     get '/current_user', to: 'sessions#logged_in?'
+    
     resource :sessions
+    
+    resources :projects do 
+      resources :tasks
+    end 
 
     resources :tasks
 end
