0df0b9901e0fb3ec7401dac762f54ac3c8c6bfa7
Rafael Batista <RafaelB2424@gmail.com>
2020-11-04 22:05:04 -0500

    Add update action

--- ticket-tracker-api/app/controllers/tasks_controller.rb
+++ ticket-tracker-api/app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController
+    def update 
+        task = Task.find_by(id: params[:id].to_i)
+        task.update(completed: true) 
+
+        if task.save 
+            render json: "Completed", status: :ok and return
+        end 
+
+        render json: {errors: "An error occured"}
+    end 
 end
