592dd714c2ef5f223882d6fb0202aea094221fef
Rafael Batista <RafaelB2424@gmail.com>
2020-11-04 22:11:19 -0500

    Add link to project task

--- ticket-tracker-client/src/components/Projects/ProjectsView/ProjectsView.js
+++ ticket-tracker-client/src/components/Projects/ProjectsView/ProjectsView.js
@@ -1,17 +1,18 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import { Link } from 'react-router-dom';
 import './ProjectsView.css';
 
 const ProjectsView = (props) => {
-    let { projects } = props
-    if(!projects) projects = []
-    console.log(projects)
+    let { project } = props;
     return (
-        <div className="text-center mt-5">
-            <Link>Create a Project</Link>
-            {projects.map(project => {
-                <div>project.name</div>
-            })}
+        <div className="text-center mt-5 projects-view">
+           <Link to={{
+               pathname:`/projects/${project.id}/tasks`, 
+               state: {tasks: project.tasks} }}>{
+               project.name}
+            </Link>
+           <span>{project.description}</span>
+           <span>{project.tasks.length} tasks   </span>
         </div>
     )
 }
