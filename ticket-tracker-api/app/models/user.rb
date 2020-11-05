f4d7c4748cfdbd696cba22ab6d46183681106096
Rafael Batista <RafaelB2424@gmail.com>
2020-11-04 22:06:23 -0500

    Add validations

--- ticket-tracker-api/app/models/user.rb
+++ ticket-tracker-api/app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     has_secure_password 
     has_many :projects
 
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, presence: true
+    validates :username, presence: true, uniqueness: true
+
     def self.handle_login(email, password)
         user = User.find_by(email: email.downcase)
         
