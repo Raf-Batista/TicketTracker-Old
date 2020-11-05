483ec6bb64e02ad13a6d770a89b2898898832c61
Rafael Batista <RafaelB2424@gmail.com>
2020-11-04 22:09:42 -0500

    Import task and add route

--- ticket-tracker-client/src/App.js
+++ ticket-tracker-client/src/App.js
@@ -9,6 +9,7 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Footer from './components/Footer';
 import ProjectsContainer from './components/Projects/ProjectsContainer';
+import Task from './components/Task';
 
 library.add(fab);
 
@@ -22,6 +23,7 @@ const App = () => {
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/projects" render={props => <ProjectsContainer {...props}/>} />
+          <Route exact path="/projects/:id/tasks" component={Task} />
         </Switch>
         <Footer />
       </Router>
