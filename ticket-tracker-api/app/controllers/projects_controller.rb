12230e14fe679701b6335971f533c32c48cdfd82
Rafael Batista <RafaelB2424@gmail.com>
2020-11-05 00:22:45 -0500

    Add create action

--- ticket-tracker-api/app/controllers/projects_controller.rb
+++ ticket-tracker-api/app/controllers/projects_controller.rb
@@ -1,2 +1,18 @@
 class ProjectsController < ApplicationController
+
+    def create 
+        user = User.find_by(id: params[:user_id].to_i)
+        user.projects.create(project_params)
+        if user.save 
+            render json: "Project Created", status: :ok and return
+        end     
+
+        render json: {errors: "An error occured"}
+    end 
+
+    private 
+
+    def project_params 
+        params.permit(:name, :description)
+    end 
 end
