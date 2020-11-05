e345e887bbc5eb1d76b44cd0fbde0aaf668adf35
Rafael Batista <RafaelB2424@gmail.com>
2020-11-05 00:23:09 -0500

    Add create action

--- ticket-tracker-api/app/controllers/tasks_controller.rb
+++ ticket-tracker-api/app/controllers/tasks_controller.rb
@@ -1,4 +1,15 @@
 class TasksController < ApplicationController
+
+    def create 
+        project = Project.find_by(id: params[:project_id].to_i)
+        project.tasks.create(task_params)
+        if project.save 
+            render json: "Task created", status: :ok and return 
+        end 
+
+        render json: {errors: "An error occured"}, status: :not_acceptable
+    end 
+
     def update 
         task = Task.find_by(id: params[:id].to_i)
         task.update(completed: true) 
@@ -8,5 +19,11 @@ class TasksController < ApplicationController
         end 
 
         render json: {errors: "An error occured"}
+    end 
+
+    private 
+
+    def task_params 
+        params.permit(:name, :description)
     end 
 end
