87920abec7c7dbbcdcbd909dc16c24d2761da010
Rafael Batista <RafaelB2424@gmail.com>
2020-11-08 15:41:27 -0500

    Render link and map through projects if a user is logged in

--- ticket-tracker-client/src/components/Projects/ProjectsContainer/ProjectsContainer.js
+++ ticket-tracker-client/src/components/Projects/ProjectsContainer/ProjectsContainer.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import ProjectsView from '../ProjectsView'
-import currentUser from '../../../async/currentUser';
+// import currentUser from '../../../async/currentUser';
 
 const ProjectsContainer = (props) => {
-    const { history } = props;
-    const [user, setUser] = useState({id: 0, projects: []})
+    const { history, user } = props;
+    // const [user, setUser] = useState({id: 0, projects: []})
 
-    useEffect( async () => {
-    setUser(await currentUser())
-    }, []);
+    // useEffect( async () => {
+    // setUser(await currentUser())
+    // }, []);
     return (
         <div className="text-center mt-5">
-             <Link to={`/users/${user.id}/projects/new`}>Create a Project</Link> 
-             <span className="d-block">Click on a project to view tasks</span>
-            {user.projects.map(project => (<ProjectsView project={project} />))}
+            { user ?
+                <>
+                    <Link to={`/users/${user.id}/projects/new`}>Create a Project</Link>
+                    <span className="d-block">Click on a project to view tasks</span>
+                    {
+                        user.projects.map(project => (<ProjectsView project={project} />))
+                    }
+                </>
+                : null}
         </div>
     )
 }
