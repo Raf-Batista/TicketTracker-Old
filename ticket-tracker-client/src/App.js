018ca392d854b5c5ecdc97af4c6b4572a428d7d7
Rafael Batista <RafaelB2424@gmail.com>
2020-11-08 15:40:51 -0500

    Fetch the current user in a use effect hook

--- ticket-tracker-client/src/App.js
+++ ticket-tracker-client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -12,10 +12,17 @@ import ProjectsContainer from './components/Projects/ProjectsContainer';
 import ProjectForm from './components/Projects/ProjectForm';
 import Task from './components/Tasks/Task';
 import TaskForm from './components/Tasks/TaskForm';
+import currentUser from './async/currentUser';
 
 library.add(fab);
 
 const App = () => {
+  const [user, setUser] = useState()
+
+  useEffect( async () => {
+   setUser(await currentUser()); 
+  }, []);
+
   return (
     <div className="main">
       <Router>
@@ -24,7 +31,7 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
-          <Route exact path="/projects" render={props => <ProjectsContainer {...props}/>} />
+          <Route exact path="/projects" render={props => <ProjectsContainer {...props} user={user} />} />
           <Route exact path="/users/:id/projects/new" render={props => <ProjectForm {...props}/>} />
           <Route exact path="/projects/:id/tasks" component={Task} />
           <Route exact path="/projects/:id/tasks/new" component={TaskForm} />
