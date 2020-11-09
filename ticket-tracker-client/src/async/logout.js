71593e0d8de27e977437024eb0ed47aff0a1b138
Rafael Batista <RafaelB2424@gmail.com>
2020-11-09 17:43:02 -0500

    Create script to logout

---
+++ ticket-tracker-client/src/async/logout.js
@@ -0,0 +1,26 @@
+const URL = `${process.env.REACT_APP_URL}/sessions`;
+
+const login = async (params) => {
+
+    try {
+        const response = await fetch(URL, {
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json", 
+                "Accept": "application/json",
+            },
+            method: "DELETE",
+            body: JSON.stringify(params)
+        });
+
+        const result = await response.json();
+
+        if(!result.errors) return result; 
+
+        return result.errors;
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export default login;
